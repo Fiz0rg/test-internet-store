23fc15e364449551510436dedb035b3286619835
Fiz0rg <roma_03-96@mail.ru>
2022-06-12 17:01:26 +0300

    usless import

--- endpoints/category.py
+++ endpoints/category.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-from starlette.responses import Response
 
 from schemas.category import CategorySchemas, CategorySchemasId
 from repositories.category import BaseCategoryClass
@@ -10,13 +9,13 @@ from endpoints.depends import category_repository
 category_router = APIRouter()
 
 
-@category_router.post('/create', response_model=CategorySchemasId, response_class=Response)
+@category_router.post('/create', response_model=CategorySchemasId)
 async def create_category(cat: CategorySchemas,
                           base_class: BaseCategoryClass = Depends(category_repository)):
     await base_class.create_category(cat=cat)
 
 
-@category_router.get('/get', response_model=List[CategorySchemasId], response_class=Response)
+@category_router.get('/get', response_model=List[CategorySchemasId])
 async def get_category(offset: int = 0,
                        limit: int = 10,
                        base_class: BaseCategoryClass = Depends(category_repository)):
